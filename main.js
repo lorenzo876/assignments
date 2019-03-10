f9fa1884efa1a0afa02f884eb9379b23b5cdcb6f
lorenzo876 <44174885+lorenzo876@users.noreply.github.com>
2019-03-10 18:15:47 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+class Main {
+    execute() {
+
+        let colors = []; {
+            colors.push('Red,', 'black', 'Blue');
+            console.log(colors);
+        }
+        let colors = ['Red', 'Green', 'Blue'];{
+            colors.splice(1);
+            console.log(colors);
+           }
+         
+        
+        let ages = [18, 19, 20, 51, 5, 25, 14, 31, 40, 26, 99];
+        let result = 0;
+        for (let i = 0; i < ages.length; i++) {
+            result = result + ages[i];
+        }
+            console.log(result)
+        
+        let ages = [18, 19, 20, 51, 5, 25, 14, 31, 40, 26, 99]; {
+            let odd = [19, 51, 5, 25, 31, 99]
+            let result = 0;
+            for (let i = 0; i < odd.length; i++)
+                result = result + odd[i];
+            console.log(result)
+
+        }
+        let colors1 = ["blue","white"];
+        let colors2 = ["green","red"];
+        
+        for(let i = 0; i<colors1.length; i++ ){
+         colors2.push(colors1[i]);
+         console.log(colors2);
+        }
+               
+        let colors1 = ["blue","white"];
+        let colors2 = ["green","red"];
+        
+        for(let i = 0; i<colors2.length; i++ ){
+         colors1.push(colors2[i]);
+         console.log(colors1)
+        }
+        
+
+    }
+}
+new Main().execute();
