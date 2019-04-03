4ce04784e350def5e5d5db15b9216f805610689b
lorenzo876 <44174885+lorenzo876@users.noreply.github.com>
2019-04-03 14:55:00 +0200

    Update objec.js

--- objec.js
+++ objec.js
@@ -2,31 +2,31 @@ function Main() {
     this.execute = function () {
 
 
-
+        // Exercise I
         let musicAlbum = {
             Title: 'Pon Di Gaza',
             Author: 'Vybz Kartel',
             Label: 'Adidjaheim/Notnice',
             releaseDate: 'September 22, 2009',
-            Songs: ['Want My Weed', 'Struggle', 'Life We Living','Romping Shop','Like A movie',],
+            Songs: ['Want My Weed', 'Struggle', 'Life We Living', 'Romping Shop', 'Like A movie',],
         };
         console.log(musicAlbum);
 
-
+        //Exercise II
         let newalbum = {
-			title: "pon di gaza 2",
-			artist: "vybz kartel",
-			thelabelcompanyname: "notnice",
-			thereleaseyear : "2010",
-			songs: ["ghetto people","where im from", "dont run ", "wicked people"],
-		};
+            title: "pon di gaza 2",
+            artist: "vybz kartel",
+            thelabelcompanyname: "Tad's Record",
+            thereleaseyear: "2010",
+            songs: ["can't get over me", "Mr. Officer", "GO GO Club", "Come breed me "],
+        };
 
-		// excercise 3
-		function showinfo (album2){
-			return (newalbum.title + newalbum.songs);
-		}
+        // Excercise III
+        function showinfo(newalbum) {
+            return (newalbum.title + newalbum.songs);
+        }
 
-		showinfo(newalbum);
+        showinfo(newalbum);
 
     }
 }
